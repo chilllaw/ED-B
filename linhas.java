d8c4b3239fd276097a52add1fcce30de6922729d
chilllaw <167825872+chilllaw@users.noreply.github.com>
2024-08-26 13:33:40 +0000

    alteração

--- linhas.java
+++ linhas.java
@@ -4,7 +4,6 @@ public class linhas {
 public static void main(String[]args) {
     Scanner t = new Scanner (System.in);
     int n;
-    int p = 0;
     System.out.println("Insira um número inteiro: ");
     n = t.nextInt();
     for (int i=1; i<=n; i++) {
